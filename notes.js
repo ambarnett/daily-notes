7e067c12ddcf897d7d92b981ce8ca9aa73dc11ae
ambarnett <andy.barnett09@gmail.com>
2021-05-28 16:40:22 -0500

    first commit for notes project

---
+++ notes.js
@@ -0,0 +1,41 @@
+const notes = [
+    {
+        id: 1,
+        subject: "how to better link JS to HTML and CSS",
+        date: 20210527,
+        feeling: "Frusterated",
+        timeSpent: 120
+    },
+    {
+        id: 2,
+        subject: "practice creating objects in JS",
+        date: 20210527,
+        feeling: "Good",
+        timeSpent: 30
+    }
+]
+
+const noteAboutToday = {
+    id: 3,
+    subject: "more practice with JS",
+    date: 20210528,
+    feeling: "Good",
+    timeSpent: 300
+}
+
+notes.push(noteAboutToday)
+
+const searchTerm = "Good"
+
+
+
+for (const note of notes){
+    if(note.feeling === searchTerm){
+        console.log(`Note #${note.id}
+        ${note.subject}
+        The date the note was taken: ${note.date}
+        How I was feeling after: ${note.feeling}
+        How long I was working on the subject of the note: ${note.timeSpent} minutes
+        ---------------------------------------`)
+    }
+}
